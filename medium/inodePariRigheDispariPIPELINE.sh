b2e375f6d153d5953898ec8e5af9f4b5fa18c21f
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2022-07-12 17:27:46 +0200

    Update inodePariRigheDispariPIPELINE.sh

--- medium/inodePariRigheDispariPIPELINE.sh
+++ medium/inodePariRigheDispariPIPELINE.sh
@@ -1,3 +1,5 @@
 #trovare il numero di file con inode pari e numero di righe dispari usando una pipeline su una riga
-
-find /usr/include -type f -name '*.h' | xargs -I {} ls -i {} | awk '(-bash % 2 == 0)' | cut -d ' ' -f2 | xargs -I {} wc -l {} | awk '(-bash % 2 == 1)' | cut -d ' ' -f2 | wc -w
+#prima cerco i file .h dentro la directory, li passo a xargs che ne fa una ls -i per ogni file e così ho gli inode per ogni file.
+#passo il risultato ad awk e per ogni inode pari stampo il file, con xargs ne conto le linee.
+#passo nuovamente l'ultimo risultato ad awk che per ogni file con linee dispari, lo stampa. Infine con wc -w conto quanti sono questi file.
+find /usr/include -type f -name '*.h' | xargs -I {} ls -i {} | awk '($1 % 2 == 0) {print $2}'| xargs -I {} wc -l {} | awk '($1 % 2 == 1) {print $1}'| wc -w
