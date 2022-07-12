0de86683d05fb3506f8773e7f86adc270d080c11
Roberto <roberto.antoniello@studenti.unimi.it>
2022-07-12 14:14:25 +0200

    added count

--- medium/inodePari_righeDispari.sh
+++ medium/inodePari_righeDispari.sh
@@ -1,10 +1,12 @@
 #trovare tutti i file .h presenti in un certo ramo del file system il cui inode è pari e il numero di righe è dispari
-
+count=0;
 for h in $(find /usr/include -type f -name '*.h'); do
 	inode=$(ls -i $h | cut -d ' ' -f1);
 	ln=$(wc -l $h | cut -d ' ' -f1);
 	if [ "$(expr $inode % 2)" -eq "0" ] && [ "$(expr $ln % 2)" -eq "1" ]; then
 		echo "$h inode: $inode  con numero righe: $ln"; 
+		let "count+=1"
 		printf "\n";
 	fi;
 done;
+echo $count
