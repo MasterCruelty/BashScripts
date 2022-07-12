fc27d64e19ac5b930e4921c87b639f382ac9697d
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2022-07-12 17:01:46 +0200

    Update strace.sh

--- medium/strace.sh
+++ medium/strace.sh
@@ -1,2 +1,2 @@
 #listare tutte le chiamate di sistema che hanno a che fare con la memoria durante l'esecuzione di "df -h". Ordinarle per frequenza ed estrarre la chiamata più frequente
-strace -e trace=%memory 2>&1 df -h 1>/dev/null | egrep -o '^[[:alpha:]]*' | sort | uniq -c | sort -nk1r
+strace -e trace=%memory 2>&1 df -h | cut -d "(" -f1 | sort | uniq -c | sort -nr | awk  '{print $1 " " $ 2}' | head -n1
