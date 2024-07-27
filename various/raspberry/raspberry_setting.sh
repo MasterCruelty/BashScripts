752e14cd6427d00cc39d67d0911af8734c3cf95a
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2024-07-27 13:56:19 +0200

    Update raspberry_setting.sh

--- various/raspberry/raspberry_setting.sh
+++ various/raspberry/raspberry_setting.sh
@@ -1,3 +1,5 @@
+#these few lines to reduce sd card usage
+
 #edit /etc/rsyslog.conf
 #after section ############ RULES ##########
 #add the following line
@@ -11,3 +13,9 @@ sudo dphys-swapfile uninstall
 sudo update-rc.d dphys-swapfile remove
 #if sure to not using in the near future 
 sudo apt purge  dphys-swapfile -y
+
+#check current hostname
+hostname
+sudo vim /etc/hostname
+#then edit the fil with new hostname
+reboot
