1d3547f6b8ae70ba53cf36a5f60070fb0fa3b5b9
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2024-08-02 22:29:38 +0200

    Create dwservice.sh

---
+++ various/raspberry/dwservice.sh
@@ -0,0 +1,6 @@
+#installation of dwservice on raspberry
+
+sudo chmod 755 dwagent.sh
+sudo ./dwagent/sh
+
+#select 1 to install and then provide installation code(you get it by creating an agent on dwservice.net
