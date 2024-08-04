a685ae49d7fe26fd1ba8ea3d74b458d505a9e98e
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2024-08-04 15:38:58 +0200

    Update backup-sd-raspberry.sh

--- various/raspberry/backup-sd-raspberry.sh
+++ various/raspberry/backup-sd-raspberry.sh
@@ -1,4 +1,7 @@
 sudo dd bs=4M if=/dev/mmcblk0 of=/dev/sda
 
-#backup diretto via ssh sulla macchina locale
-ssh pi@xx.x.x.x "sudo dd if=/dev/mmcblk0 bs=1M status=progress | gzip -" | dd of=~/Desktop/pibackup.gz
+#backup diretto via ssh sulla macchina locale (LINUX)
+ssh pi@xx.x.x.x "sudo dd if=/dev/mmcblk0 bs=4M status=progress | gzip -" | dd of=~/Desktop/pibackup.gz
+#(WINDOWS)
+ssh pi@xx.x.x.x "sudo dd if=/dev/mmcblk0 bs=4M status=progress | gzip -" | Out-File -FilePath C:\Users\<YourUsername>\Desktop\pibackup.gz -Encoding Byte
+
