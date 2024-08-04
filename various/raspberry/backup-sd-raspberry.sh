69a7f5b3be71475953543c4ca210f5cb8a621013
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2024-08-04 15:47:58 +0200

    Update backup-sd-raspberry.sh

--- various/raspberry/backup-sd-raspberry.sh
+++ various/raspberry/backup-sd-raspberry.sh
@@ -5,3 +5,18 @@ ssh pi@xx.x.x.x "sudo dd if=/dev/mmcblk0 bs=4M status=progress | gzip -" | dd of
 #(WINDOWS)
 ssh pi@xx.x.x.x "sudo dd if=/dev/mmcblk0 bs=4M status=progress | gzip -" | Out-File -FilePath C:\Users\<YourUsername>\Desktop\pibackup.gz -Encoding Byte
 
+#HOW TO RESTORE BACKUP COPIED BY SSH
+
+#LINUX
+#extract gz
+gunzip ~/Desktop/pibackup.gz
+#write image extracted to sd
+sudo dd if=~/Desktop/pibackup of=/dev/sdX bs=4M status=progress
+#verify and sync
+sync
+
+#WINDOWS
+#Extract .gz
+#Write image to sd
+#Use balenaEtcher
+#Click flash on balenaEtcher
